a4f6f2cda4e566beb903bd4f4a8fb2917183e246
Suri Mahidhar <mahidharsuri07@gmail.com>
2024-12-13 20:43:53 +0530

    How to API Call

---
+++ Api-Call/ApiCall.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+const ApiCall = (max) => {
+    const [data,setData] = useState([]);
+    const arr = ["max","ishu","janu","mahi","raj","chitti"]
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+  
+  return (<>
+           <p>Po ra rey {max.name}</p>
+           
+           <div>{data.map(item => <li key={item.id}>{item.title}---{item.userId}</li>)}
+           </div>
+           <div>{arr[3]}</div>
+           <div>{arr.map((list,index) => <li key={index}>{list}</li>)}</div>
+        
+           </>
+              
+         );
+   
+   
+};
+
+export default ApiCall;
---
+++ Api-Call/App.jsx
@@ -0,0 +1,14 @@
+
+import ApiCall from "./repository/ApiCall";
+
+
+function App(){
+  return(
+      <>
+      <ApiCall name="Mahidhar" age="22"/>
+      
+      </>
+  );
+} 
+
+export default App; 
