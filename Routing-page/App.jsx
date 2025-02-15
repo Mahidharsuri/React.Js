312497c2f3519acfe9e752fa5a000e359a2792a7
Suri Mahidhar <mahidharsuri07@gmail.com>
2025-02-15 22:58:41 +0530

    App.jsx

--- Routing-page/App.jsx
+++ Routing-page/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
        <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/about" element={<About/>}/>
+        <Route path="/about"  element={<About/>}/>
        </Routes>
      </BrowserRouter>
     </div>
